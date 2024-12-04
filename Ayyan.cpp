59f2df3b4f63f3a09e095dd0767344a2fbaf5b4a
CS Lab3  Computer Science Lab3 <cslab3@shu.edu.pk>
2024-12-04 14:21:24 +0500

    Done

---
+++ Ayyan.cpp
@@ -0,0 +1,32 @@
+#include<iostream
+using namespace std;
+int main(){
+	int choice; income,expense;
+	cin>>Type your budget;
+	switch{choice
+
+		case 1:
+			cin>>"Add income";
+			cout<<"Your balance";
+		case 2:
+			cin>>"Enter expenses";
+		    cout<<"Your expenses";
+		case 3:
+		    cin>>"Remaining Balance";
+			cout<<"Balance in your account"
+	if{
+	cin>>"Balance=expenses";
+	else{
+	cout<<"Balance is equal to the expenses";
+	else if{
+		cout<<"your balance is insuffient";
+		
+	}
+	  
+	
+}
+	
+	
+	
+	return 0;
+}
